4dec0848d6ddf0a1c2c64a46c4e0ab12026ae669
wayne warren <wayne.warren.s@gmail.com>
2023-11-23 00:11:04 -0700

    templates: add testdata and test case validating default template

--- src/templates.rs
+++ src/templates.rs
@@ -1,1 +1,23 @@
 pub const DEFAULT: &'static str = include_str!("../templates/general-purpose.typ");
+
+#[cfg(test)]
+mod test {
+    use super::super::errors::Error;
+    use super::super::generator::Generator;
+    use super::super::themes;
+    use super::*;
+
+    const SAMPLE_RESUME: &'static str = include_str!("../testdata/sample.resume.json");
+
+    #[test]
+    fn test_default() -> Result<(), Error> {
+        let g = Generator {
+            typst_source: DEFAULT.to_string(),
+            resume: serde_json::from_str(SAMPLE_RESUME)?,
+            theme: themes::DEFAULT.try_into()?,
+            output_file: None,
+        };
+        g.generate()?;
+        Ok(())
+    }
+}
